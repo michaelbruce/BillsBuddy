ee6b171aa44711d45e03a639738d4246c8937588
Michael Bruce <michael.bruce@singletracksystems.com>
2015-09-23 08:37:31 +0100

    Added TODOs

--- billsbuddy.py
+++ billsbuddy.py
@@ -1,17 +1,25 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 
 settings = sublime.load_settings('billsbuddy.sublime-settings')
 plugin_path = sublime.packages_path()
+home_dir = os.path.expansuser('~')
 
-import subprocess
-from subprocess import Popen,PIPE
-import os
-
+# TODO use os.putenv to find & set JAVAHOME?
+# TODO find tooling-force jar with a methid - os.listdir('.')
 
 class ToolingForce:
     def call(args):
-        p = subprocess.Popen("echo hiiii")
-        p.communicate() #now wait
+        # TODO create response file from current file name
+        # TODO end current tooling-force process if call is used again.
+        # TODO how to reference $HOME in python?
+        p = os.popen("echo hiiii")
+        os.popen(("java -jar tooling-force.com-0.3.4.2.jar"
+            " --config=" + home_dir + settings.get('bb_config')
+            " --projectPath=" + home_dir + settings.get('bb_path')
+            " --responseFilePath=/tmp/billResponseFile"
+            " --ignoreConflicts=true"
+            " --pollWaitMillis=1000")
+        #now wait & print
 
 class BillHelpCommand(sublime_plugin.TextCommand):
     def run(self, edit):
