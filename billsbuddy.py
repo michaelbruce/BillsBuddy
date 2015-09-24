dd4d7ca0da1a7306c099145cc1d89bcb98ba28b2
Michael Bruce <michael.bruce@singletracksystems.com>
2015-09-24 10:07:13 +0100

    Added threading

--- billsbuddy.py
+++ billsbuddy.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, os, subprocess
+import sublime, sublime_plugin, os, subprocess, threading
 
 settings = sublime.load_settings('billsbuddy.sublime-settings')
 plugin_path = sublime.packages_path() + '/BillsBuddy'
@@ -9,15 +9,20 @@ home_dir = os.path.expanduser('~')
 # TODO create response file from current file name
 # TODO end current tooling-force process if call is used again.
 
-class ToolingForce:
-    def call(args):
+class ToolingForce(threading.Thread):
+    def __init__(self, args):
+        super(ToolingForce, self).__init__()
+        self.args = args
+
+    def run(self):
+        print('got here')
         command_args = ['java', '-jar', 'tooling-force.com-0.3.4.2.jar',
                         '--config=' + settings.get('bb_config'),
                         '--projectPath=' + settings.get('bb_path'),
                         '--responseFilePath=/tmp/billResponseFile',
                         '--ignoreConflicts=true',
                         '--pollWaitMillis=1000']
-        command_args.extend(args.split())
+        command_args.extend(self.args.split())
         print('Debug: ' + ' '.join(command_args))
         p = subprocess.Popen(command_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=plugin_path)
 
@@ -32,18 +37,20 @@ class ToolingForce:
 
 class BillSaveCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        ToolingForce.call('--action=deploySpecificFiles')
+        ToolingForce('--action=deploySpecificFiles')
 
 class BillTestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        ToolingForce.call('--action=runTestsTooling --async=true --testsToRun='
+        t = ToolingForce('--action=runTestsTooling --async=true --testsToRun='
                 + os.path.splitext(os.path.basename(self.view.file_name()))[0])
+        t.start()
 
 class BillTestSingleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        ToolingForce.call('--action=runTestsTooling --async=true --testsToRun='
+        t = ToolingForce('--action=runTestsTooling --async=true --testsToRun='
                 + os.path.splitext(os.path.basename(self.view.file_name()))[0]
                 + '.' + self.get_current_function(self.view))
+        t.start()
 
     def get_current_function(self, view):
             sel = view.sel()[0]
